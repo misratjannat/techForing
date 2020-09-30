c864be295d646caa4ec779edbe4ec90a9531112e
Misrat Mitu <misratmitu@gmail.com>
2020-09-30 21:57:36 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+$('.featured-logo-item .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+         0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.review .owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('.cyber-security .owl-carousel').owlCarousel({
+    loop:true,
+    margin:40,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+
+$('.mobile-case-study .owl-carousel').owlCarousel({
+    loop:true,
+    margin:40,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
